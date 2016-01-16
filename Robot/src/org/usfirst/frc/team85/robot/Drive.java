ac825df09a4c900e1b3698343380315dab607431
Rarstyak <9724@stu.zps.org>
2016-01-16 09:50:38 -0500

    There are only two encoders

--- Robot/src/org/usfirst/frc/team85/robot/Drive.java
+++ Robot/src/org/usfirst/frc/team85/robot/Drive.java
@@ -16,47 +16,34 @@ public class Drive {
     private CANTalon _midRightMotor;
     private CANTalon _backRightMotor;
 
-    private Encoder _frontLeftEncoder;
-    private Encoder _midLeftEncoder;  
-    private Encoder _backLeftEncoder;
-    
-    private Encoder _frontRightEncoder;
-    private Encoder _midRightEncoder;
-    private Encoder _backRightEncoder;
+    private Encoder _LeftEncoder;
+    private Encoder _RightEncoder;
 
     public Drive(Joystick drivecontroller) {
         _controller = drivecontroller
         _frontLeftMotor = new CANTalon(Addresses.LEFT_FRONT_MOTOR);
-        _frontRightMotor = new CANTalon(Addresses.RIGHT_FRONT_MOTOR);
         _midLeftMotor = new CANTalon(Addresses.LEFT_MID_MOTOR);
-        _midRightMotor = new CANTalon(Addresses.RIGHT_MID_MOTOR);
         _backLeftMotor = new CANTalon(Addresses.LEFT_BACK_MOTOR);
+        _frontRightMotor = new CANTalon(Addresses.RIGHT_FRONT_MOTOR);
+        _midRightMotor = new CANTalon(Addresses.RIGHT_MID_MOTOR);
         _backRightMotor = new CANTalon(Addresses.RIGHT_BACK_MOTOR);
 
-        _frontLeftEncoder = new Encoder(Addresses.FRONT_LEFT_ENCODER_CHANNEL_A,
-                    Addresses.FRONT_LEFT_ENCODER_CHANNEL_B);
-        _frontRightEncoder = new Encoder(Addresses.FRONT_RIGHT_ENCODER_CHANNEL_A,
-                    Addresses.FRONT_RIGHT_ENCODER_CHANNEL_B);
-        _midLeftEncoder = new Encoder(Addresses.MID_LEFT_ENCODER_CHANNEL_A,
-                    Addresses.MID_LEFT_ENCODER_CHANNEL_B);
-        _midRightEncoder = new Encoder(Addresses.MID_RIGHT_ENCODER_CHANNEL_B,
-                    Addresses.MID_RIGHT_ENCODER_CHANNEL_B);
-        _backLeftEncoder = new Encoder(Addresses.BACK_LEFT_ENCODER_CHANNEL_B,
-                    Addresses.BACK_LEFT_ENCODER_CHANNEL_B);
-        _backRightEncoder = new Encoder(Addresses.BACK_RIGHT_ENCODER_CHANNEL_A,
-                    Addresses.BACK_RIGHT_ENCODER_CHANNEL_B);
+        _LeftEncoder = new Encoder(Addresses.LEFT_ENCODER_CHANNEL_A,
+                    Addresses.LEFT_ENCODER_CHANNEL_B);
+        _RightEncoder = new Encoder(Addresses.RIGHT_ENCODER_CHANNEL_A,
+                    Addresses.RIGHT_ENCODER_CHANNEL_B);
 
         //_drive = new RobotDrive(_frontLeftMotor, _midLeftMotor, _backLeftMotor, _frontRightMotor, _midRightMotor, _backRightMotor);
     }
     
     public void drive() {
-        double controllerR = _controller.getRawAxis(4);
         double controllerL = _controller.getRawAxis(1);
+        double controllerR = _controller.getRawAxis(4);
         // // Adjustment of values
-        setMotors(controllerR, controllerL);
+        setMotors(controllerL, controllerR);
     }
     
-    private void setMotors(double rSpeed, double lSpeed) {
+    private void setMotors(double lSpeed, double rSpeed) {
         _frontLeftMotor.set(lSpeed);
         _midLeftMotor.set(lSpeed);
         _backLeftMotor.set(lSpeed);
