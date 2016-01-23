767df0ae5fe8675fac024b423c0bf20775351903
Matthew Chau <nighton55infit@live.com>
2016-01-23 13:48:12 -0500

    Autonomous Frame
    need to setup controller with buttons for different obstacle presets and
    the different classes that will be called and do each different obstacle

--- Robot/src/org/usfirst/frc/team85/robot/Auto.java
+++ Robot/src/org/usfirst/frc/team85/robot/Auto.java
@@ -1,11 +1,36 @@
 package org.usfirst.frc.team85.robot;
 
 import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class Auto {
 	private TankDrive _drive;
-
-    public Auto(TankDrive drive) {
+	
+	/* private JoystickButton _controller2;
+		_controller2 = Button; */
+	
+	public Auto(TankDrive drive) {
         _drive = drive;
-    }
+        
+        /*if (controller2.getRawButton(1) == true) {
+        	LowBar.AutonomousLowBar();
+		} else if (controller2.getRawButton(2) == true) {
+			Portcullis.main();
+		} else if (controller2.getRawButton(3) == true) {
+			ChevalDeFrise.main();
+		} else if (controller2.getRawButton(4) == true) {
+			Moat.main();
+		} else if (controller2.getRawButton(5) == true) {
+			Ramparts.main();
+		} else if (controller2.getRawButton(6) == true) {
+			Drawbridge.main();
+		} else if (controller2.getRawButton(7) == true) {
+			SallyPort.main();
+		} else if (controller2.getRawButton(8) == true) {
+			RockWall.main();
+		} else if (controller2.getRawButton(9) == true) {
+			RoughTerrain.main();
+		} */
+	}
 }
