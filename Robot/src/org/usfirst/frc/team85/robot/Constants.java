faeca6633fa9c2bda4a8e4c538065c6600c46bc9
Colton Bates <snsiox@gmail.com>
2016-01-23 12:03:39 -0500

    Made constants Public
    :/

--- Robot/src/org/usfirst/frc/team85/robot/Constants.java
+++ Robot/src/org/usfirst/frc/team85/robot/Constants.java
@@ -2,13 +2,13 @@ package org.usfirst.frc.team85.robot;
 
 public class Constants {
     //PID constants
-    private static final double shooterP = 0.0;
-    private static final double shooterI = 0.0;
-    private static final double shooterD = 0.0;
-    private static final double shooterF = 0.0;
+    public static final double shooterP = 0.0;
+    public static final double shooterI = 0.0;
+    public static final double shooterD = 0.0;
+    public static final double shooterF = 0.0;
 
-    private static final double armP = 0.0;
-    private static final double armI = 0.0;
-    private static final double armD = 0.0;
-    private static final double armF = 0.0;
+    public static final double armP = 0.0;
+    public static final double armI = 0.0;
+    public static final double armD = 0.0;
+    public static final double armF = 0.0;
 }
