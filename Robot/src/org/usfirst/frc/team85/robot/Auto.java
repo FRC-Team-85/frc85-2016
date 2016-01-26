53c809e66dcbde9692e1cae1fe829a7dd0200aa8
Nathan W <bluroseai@hotmail.com>
2016-01-26 17:28:52 -0500

    Decided to use a switch statement with cases.
    Matthew's else-if still commented out.  I'd like to actually do something on this tonight...
    This assumes a separate method for each obstacle, is this how we were going to do this?
    luv ya fam <3

--- Robot/src/org/usfirst/frc/team85/robot/Auto.java
+++ Robot/src/org/usfirst/frc/team85/robot/Auto.java
@@ -12,6 +12,36 @@ public class Auto {
 	
 	public Auto(TankDrive drive) {
         _drive = drive;
+        switch (obstacle) {
+        case 0://LowBar
+            
+            break;
+        case 1://Portcullis
+        
+            break;
+        case 2://Cheval de Frise
+            
+            break;
+        case 3://Moat
+        
+            break;
+            
+        case 4://Ramparts
+        
+            break;
+            
+        case 5://Drawbridge
+            
+            break;
+        case 6://Sally Port
+            
+            break;
+        case 7://Rock Wall
+            
+            break;
+        case 8://Rough Terrain
+            
+            break;
         
         /* if (controller2.getRawButton(1) == true) {
         	LowBar.AutonomousLowBar();
