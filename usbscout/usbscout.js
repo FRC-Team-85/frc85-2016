faceafbd5c44414f3287fd7421d7e4ac5e8bd3f0
Colton Bates <snsiox@gmail.com>
2016-02-19 17:26:52 -0500

    Addition of USBScout
    Why does node.js make everything have so many files ლ(ಠ益ಠლ)

---
+++ usbscout/usbscout.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+
+var redis = require('redis');
+var client = redis.createClient('/tmp/redis.sock');
+
+var fields = ["a_reach"]
+
+app.get('/', function(req, res) {
+    res.send("Default page");
+});
+
+app.get('/api/:teamid/get', function(req, res) {
+    var teamid = res.teamid;
+    client.hgetall("team:85", function(err, reply) {
+        if (err) {
+            res.json({});
+            res.status(500).json({ error: "It broke."});
+        } else {
+            res.json(reply);
+        }
+    });
+});
+
+app.get('/api/:teamid/set', function(req, res) {
+  var teamid = res.teamid;
+  for (var i = 0; i < fields.length; i++) {
+      var value = (req.param(fields[i]));
+      client.hset("team:85", fields[i], value);
+      res.json(req.params);
+      console.log(JSON.stringify(req.params));
+  }
+});
+
+app.listen(80, function() {
+    console.log('Server is up');
+});
